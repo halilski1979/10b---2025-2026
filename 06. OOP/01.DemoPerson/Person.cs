a5f84ede6e8e1ae9e92a9afb06b8144421ac6292
Halil <dospatliyski@gmail.com>
2026-04-15 10:18:11 +0300

    wew

---
+++ 06. OOP/01.DemoPerson/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.DemoPerson
+{
+    public class Person
+    {
+		//поле - field
+		private string name;
+        private int age;
+        private string gender;
+        private double salary;
+
+        //свойство - property
+        public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}		
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}		
+		public string Gender
+		{
+			get { return gender; }
+			set { gender = value; }
+		}			
+		public double Salary
+		{
+			get { return salary; }
+			set { salary = value; }
+		}
+
+
+		public void Print()
+		{
+            Console.WriteLine($"Kazvam se {Name}, na {Age} godini. Zaplata: {Salary:f2} EURO");
+		}
+
+		public void IncreaseSalary(double percent)
+		{
+			Salary = Salary * percent + Salary;
+		}
+
+
+
+	}
+}
---
+++ 06. OOP/01.DemoPerson/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _01.DemoPerson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Ivan";
+            p.Age = 25;
+            p.Gender = "male";
+            p.Salary = 2250.55;
+
+            Person q= new Person();
+            q.Name = "Petq";
+            q.Age = 35;
+            q.Gender = "female";
+            q.Salary = 1500.34;
+
+
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}. Gender: {p.Gender}. Salary: {p.Salary} EURO");
+            Console.WriteLine($"Name: {q.Name}, Age: {q.Age}. Gender: {q.Gender}. Salary: {q.Salary} EURO");
+
+            Console.WriteLine();
+            p.Print();
+            q.Print();
+
+            p.IncreaseSalary(0.15);
+            q.IncreaseSalary(0.15);
+
+            Console.WriteLine();
+            p.Print();
+            q.Print();
+
+        }
+    }
+}
